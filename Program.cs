e9783313974527f2876b465401afa0be7a79e771
MaksBlyshchyk <blyshchyk_ak24@nuwm.edu.ua>
2025-10-16 14:13:38 +0300

    4

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LabWork
     // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
     // Необхідно змінювати і дописувати код лише в цьому проекті
     // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
+     // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
     class Program
     {
         static void Main(string[] args)
